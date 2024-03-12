7adb2d2278dea226218f4a719f8f7607691ba4c9
imloryxx <lorytognati88@gmail.com>
2024-03-12 08:43:30 +0100

    V2.0

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 const bookButton = document.getElementById('bookButton');
-const bookingForm = document.getElementById('bookingForm');
+const bookingFormSection = document.getElementById('bookingForm');
 const homeButton = document.getElementById('homeButton');
 const infoButton = document.getElementById('infoButton');
 const infoPopup = document.getElementById('infoPopup');
 
 bookButton.addEventListener('click', () => {
-    bookingForm.style.display = 'block';
+    bookingFormSection.style.display = 'block';
     homeButton.style.display = 'none';
     infoButton.style.display = 'none';
     infoPopup.style.display = 'none';
 });
 
 homeButton.addEventListener('click', () => {
-    bookingForm.style.display = 'none';
+    bookingFormSection.style.display = 'none';
     homeButton.style.display = 'block';
     infoButton.style.display = 'block';
     infoPopup.style.display = 'none';
 });
 
 infoButton.addEventListener('click', () => {
-    bookingForm.style.display = 'none';
+    bookingFormSection.style.display = 'none';
     homeButton.style.display = 'block';
     infoButton.style.display = 'block';
     infoPopup.style.display = 'block';
 });
 
-// JavaScript per il reindirizzamento dopo la compilazione del modulo di prenotazione
-// Aggiungi questo codice dopo la gestione dell'invio del modulo nella tua pagina HTML
-window.location.href = 'https://docs.google.com/spreadsheets/d/1kAHN5afQ1BGnDc_m6vIo-2mgKH5aCsWivJunajaSRKs/edit?usp=sharing';
-
+// Gestione dell'invio del modulo di prenotazione
+const bookingForm = document.querySelector('.booking-form form');
+bookingForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Evita il comportamento predefinito di invio del modulo
+    // Esegui le operazioni di invio del modulo
+    window.location.href = 'https://docs.google.com/spreadsheets/d/1kAHN5afQ1BGnDc_m6vIo-2mgKH5aCsWivJunajaSRKs/edit?usp=sharing'; // Reindirizza dopo l'invio del modulo
+});
