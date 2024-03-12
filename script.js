245c45072b8ca1dcc8868dc8d76cd3724a78023c
imloryxx <lorytognati88@gmail.com>
2024-03-12 08:33:06 +0100

    V2

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-function showCity(cityName) {
-    // Nascondi tutte le città
-    var cities = document.querySelectorAll('.city');
-    cities.forEach(city => {
-        city.classList.remove('active');
-    });
+const bookButton = document.getElementById('bookButton');
+const bookingForm = document.getElementById('bookingForm');
+const homeButton = document.getElementById('homeButton');
+const infoButton = document.getElementById('infoButton');
+const infoPopup = document.getElementById('infoPopup');
 
-    // Mostra la città selezionata
-    var city = document.getElementById(cityName);
-    city.classList.add('active');
-}
+bookButton.addEventListener('click', () => {
+    bookingForm.style.display = 'block';
+    homeButton.style.display = 'none';
+    infoButton.style.display = 'none';
+    infoPopup.style.display = 'none';
+});
+
+homeButton.addEventListener('click', () => {
+    bookingForm.style.display = 'none';
+    homeButton.style.display = 'block';
+    infoButton.style.display = 'block';
+    infoPopup.style.display = 'none';
+});
+
+infoButton.addEventListener('click', () => {
+    bookingForm.style.display = 'none';
+    homeButton.style.display = 'block';
+    infoButton.style.display = 'block';
+    infoPopup.style.display = 'block';
+});
+
+// JavaScript per il reindirizzamento dopo la compilazione del modulo di prenotazione
+// Aggiungi questo codice dopo la gestione dell'invio del modulo nella tua pagina HTML
+window.location.href = 'https://docs.google.com/spreadsheets/d/1kAHN5afQ1BGnDc_m6vIo-2mgKH5aCsWivJunajaSRKs/edit?usp=sharing';
+
